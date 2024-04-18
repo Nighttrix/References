d2bc6faea7c0a40f8311b53fb5c74cbfc8be0e30
Nighttrix <voodoo_child1969@protonmail.com>
2024-04-17 22:22:06 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+MoonRover lunkhod = new MoonRover("Lunokhod 1", 1970);
+MoonRover apollo = new MoonRover("Apollo", 1971);
+MoonRover sojourner = new MoonRover("Sojourner", 1997);
+MoonRover sputnik = new MoonRover("Sputnik", 1957);
+
